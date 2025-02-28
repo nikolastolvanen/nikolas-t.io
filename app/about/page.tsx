052cec70b6218ea2c1b26b6e7d862a2254fd24c5
Nikolas Tolvanen <nikolas.tolvanen@gmail.com>
2025-02-28 20:39:32 +0200

    Fix image layout issue on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ export default async function About() {
     <section className='pb-24 pt-16'>
       <div className='container max-w-[720px]'>
         <h2 className='title'>About</h2>
-        <div className='mt-8 flex flex-row gap-8'>
+        <div className='mt-8 flex flex-col gap-8 sm:flex-row'>
           <div className='w-full sm:w-1/2'>
             <p className='pb-2'>
               Hello! My name is Nikolas Tolvanen. I&#39;m from Joensuu, Finland.
@@ -27,31 +27,27 @@ export default async function About() {
               I&#39;m hoping to run a marathon soon in the future.
             </p>
           </div>
-          <div className='invisible sm:visible'>
-            <Image
-              className='absolute ml-[30px] flex-1 rotate-[0.1rad] rounded-lg'
-              src='/images/gallery/helsinki.jpg'
-              alt='Nikolas in Helsinki'
-              width={200}
-              height={100}
-              priority
-            />
-            <Image
-              className='absolute mt-[260px] flex-1 rotate-[-0.05rad] rounded-lg'
-              src='/images/gallery/uef.jpg'
-              alt='University of Eastern Finland'
-              width={300}
-              height={100}
-              priority
-            />
-            {/* <Image
-              className='absolute ml-[80px] mt-[395px] flex-1 rotate-[0.03rad] rounded-lg'
-              src='/images/gallery/vaticancity.jpg'
-              alt='Nikolas in Vatican City'
-              width={200}
-              height={175}
-              priority
-            /> */}
+          <div className='flex flex-row sm:flex-col'>
+            <div>
+              <Image
+                className='rotate-[0.1rad] rounded-lg'
+                src='/images/gallery/helsinki.jpg'
+                alt='Nikolas in Helsinki'
+                width={200}
+                height={100}
+                priority
+              />
+            </div>
+            <div>
+              <Image
+                className='rotate-[-0.05rad] rounded-lg'
+                src='/images/gallery/uef.jpg'
+                alt='University of Eastern Finland'
+                width={300}
+                height={100}
+                priority
+              />
+            </div>
           </div>
         </div>
       </div>
