bab0b5c5946dad71b2c9ae552db37f6254eee4fc
Nikolas Tolvanen <nikolas.tolvanen@gmail.com>
2025-03-02 12:15:47 +0200

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,18 @@
-function ArrowIcon() {
+function LinkIcon() {
   return (
     <svg
-      width='12'
-      height='12'
-      viewBox='0 0 12 12'
       fill='none'
+      height='18'
+      viewBox='0 0 24 24'
+      width='18'
       xmlns='http://www.w3.org/2000/svg'
     >
       <path
-        d='M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z'
-        fill='currentColor'
+        d='m10 6h-4c-1.10457 0-2 .89543-2 2v10c0 1.1046.89543 2 2 2h10c1.1046 0 2-.8954 2-2v-4m-4-10h6m0 0v6m0-6-10 10'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2'
       />
     </svg>
   )
@@ -18,14 +21,14 @@ function ArrowIcon() {
 export default function Footer() {
   return (
     <footer className='py-8'>
-      <div className='container flex max-w-[720px] flex-row items-center justify-between space-x-10 sm:justify-start'>
+      <div className='container flex max-w-[720px] flex-row items-end justify-between sm:items-center sm:justify-start sm:space-x-16'>
         <div>
           <p className='text-sm leading-5 text-muted-foreground'>
             {new Date().getFullYear()} - Nikolas Tolvanen
           </p>
         </div>
         <div>
-          <ul className='flex flex-row space-x-4'>
+          <ul className='sm-items-center flex flex-col items-end space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0'>
             <li>
               <a
                 className='flex items-center text-muted-foreground transition-all hover:text-foreground'
@@ -33,8 +36,8 @@ export default function Footer() {
                 target='_blank'
                 href='/rss'
               >
-                <ArrowIcon />
-                <p className='ml-2 h-7'>rss</p>
+                <p className='ml-2 text-sm'>RSS</p>
+                <LinkIcon />
               </a>
             </li>
             <li>
@@ -44,8 +47,8 @@ export default function Footer() {
                 target='_blank'
                 href='https://github.com/nikolastolvanen/nikolas-t.io'
               >
-                <ArrowIcon />
-                <p className='ml-2 h-7'>repo</p>
+                <p className='ml-2 text-sm'>GitHub Repo</p>
+                <LinkIcon />
               </a>
             </li>
           </ul>
